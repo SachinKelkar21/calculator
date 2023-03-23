66854f75b566fa81defccc60baaa38fd43967b59
SachinKelkar21 <sachin.kelkar79@gmail.com>
2023-03-23 12:54:39 +0530

    dd

--- src/main/java/com/sachin/calculator/Calculator.java
+++ src/main/java/com/sachin/calculator/Calculator.java
@@ -1,7 +1,7 @@
 package com.sachin.calculator;
 
 import org.springframework.stereotype.Service;
-/* this i main class*/
+/* this i main class */
 @Service
 public class Calculator {
 	int sum(int a, int b) {
